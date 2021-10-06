6275465bf9d79b8370a52fd70645aa3bc3765caf
Roland <90884311+EnvironmentCommittee@users.noreply.github.com>
2021-10-06 23:05:49 +0800

    Add files via upload

---
+++ Group1.py
@@ -0,0 +1,40 @@
+def function1(): print('function1') #To be edited by member 1, Replace this line with your actual function code
+
+def function2(): print('function2') #To be edited by member 2, Replace this line with your actual function code
+
+def function3(): print('function3') #To be edited by member 3, Replace this line with your actual function code
+
+def function4(): print('function4') #To be edited by member 4, Replace this line with your actual function code
+
+print('This is ENG1003'' Week 1 Tutorial Programming Task') inp = input() #Ask for an integer
+
+if inp == '1': function1(
+print('Yanka')
+print('Kelvin')
+print('Roland')
+print('Lian jiarong')
+print('Fu Chen Lei')
+) 
+elif inp == '2': function2(
+a=1
+b=3
+c=4
+print(a)
+print(b)
+print(c)
+print(a+b+c)) 
+elif inp == '3': function3(
+a = 5
+print (a,a**2)
+)
+ elif 
+inp == '4': function4(
+a = 1
+b = 2
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+  ) else: print('There is no function', inp)
