b6533552bf4a09a96bb58dd4b6f45fe44f3b83fd
Roland <90884311+EnvironmentCommittee@users.noreply.github.com>
2021-10-06 22:59:26 +0800

    Add files via upload

---
+++ GroupLeader.py
@@ -0,0 +1,11 @@
+def function1(): print('function1') #To be edited by member 1, Replace this line with your actual function code
+
+def function2(): print('function2') #To be edited by member 2, Replace this line with your actual function code
+
+def function3(): print('function3') #To be edited by member 3, Replace this line with your actual function code
+
+def function4(): print('function4') #To be edited by member 4, Replace this line with your actual function code
+
+print('This is ENG1003'' Week 1 Tutorial Programming Task') inp = input() #Ask for an integer
+
+if inp == '1': function1() elif inp == '2': function2() elif inp == '3': function3() elif inp == '4': function4() else: print('There is no function', inp)
